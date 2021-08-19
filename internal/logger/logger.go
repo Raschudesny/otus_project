a593a1f03228d6630fdb69165eadebd8cd414430
Raschudesny <rifkinsm@gmail.com>
2021-08-19 08:48:55 +0300

    fix bad import names which belongs after project package moves

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -9,16 +9,16 @@ import (
 )
 
 func InitLogger(loggerConfig config.LoggerConfig) error {
-	config := zap.NewProductionConfig()
+	cnf := zap.NewProductionConfig()
 
-	config.EncoderConfig.EncodeDuration = zapcore.MillisDurationEncoder
-	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	config.ErrorOutputPaths = []string{"stderr"}
-	config.OutputPaths = []string{loggerConfig.File, "stdout"}
-	if err := config.Level.UnmarshalText([]byte(loggerConfig.Level)); err != nil {
+	cnf.EncoderConfig.EncodeDuration = zapcore.MillisDurationEncoder
+	cnf.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	cnf.ErrorOutputPaths = []string{"stderr"}
+	cnf.OutputPaths = []string{loggerConfig.File, "stdout"}
+	if err := cnf.Level.UnmarshalText([]byte(loggerConfig.Level)); err != nil {
 		return fmt.Errorf("error building logger, can't parse logger level from file: %w", err)
 	}
-	currentLogger, err := config.Build()
+	currentLogger, err := cnf.Build()
 	if err != nil {
 		return fmt.Errorf("error building logger: %w", err)
 	}
--- internal/server/mappers.go
+++ internal/server/mappers.go
@@ -1,26 +1,26 @@
 package server
 
 import (
-	pb2 "github.com/Raschudesny/otus_project/v1/internal/server/pb"
-	storage2 "github.com/Raschudesny/otus_project/v1/internal/storage"
+	"github.com/Raschudesny/otus_project/v1/internal/server/pb"
+	"github.com/Raschudesny/otus_project/v1/internal/storage"
 )
 
-func MapBannerToPb(banner storage2.Banner) *pb2.Banner {
-	return &pb2.Banner{
+func MapBannerToPb(banner storage.Banner) *pb.Banner {
+	return &pb.Banner{
 		Id:          banner.ID,
 		Description: banner.Description,
 	}
 }
 
-func MapSlotToPb(slot storage2.Slot) *pb2.Slot {
-	return &pb2.Slot{
+func MapSlotToPb(slot storage.Slot) *pb.Slot {
+	return &pb.Slot{
 		Id:          slot.ID,
 		Description: slot.Description,
 	}
 }
 
-func MapGroupToPb(group storage2.SocialGroup) *pb2.Group {
-	return &pb2.Group{
+func MapGroupToPb(group storage.SocialGroup) *pb.Group {
+	return &pb.Group{
 		Id:          group.ID,
 		Description: group.Description,
 	}
